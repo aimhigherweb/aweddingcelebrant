2f7d59d9253588cdd3d4fcb2d6c4f22287ecd7ad
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-08-24 20:38:52 +0800

    added the svg plguin

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import aimhigherLogo from '../images/aimhigher.svg';
+import AimhigherLogo from '../images/aimhigher.svg';
 
 import './footer.scss';
 
@@ -15,7 +15,7 @@ const Footer = () => (
         <nav>
             <a href="https://www.facebook.com/JanneMartinCelebrant/" target="_blank" rel="nofollow noreferrer noopener">Facebook</a>
         </nav>
-        <img src={aimhigherLogo} alt="Logo for AimHigher Web Design" />
+        <AimhigherLogo />
   </footer>
 )
 
