c58e46060ffed8365e6e073072428303562392fa
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-08-24 22:13:18 +0800

    responsive footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import {Facebook} from 'react-feather'
 
 import AimhigherLogo from '../images/aimhigher.svg';
 
@@ -12,10 +13,10 @@ const Footer = () => (
             <Link to="/about">About</Link>
             <Link to="/contact">Contact</Link>
         </nav>
-        <nav>
-            <a href="https://www.facebook.com/JanneMartinCelebrant/" target="_blank" rel="nofollow noreferrer noopener">Facebook</a>
+        <nav className="social">
+            <a href="https://www.facebook.com/JanneMartinCelebrant/" target="_blank" rel="nofollow noreferrer noopener"><Facebook /></a>
         </nav>
-        <AimhigherLogo />
+        <a href="https://aimhigherweb.design" className="aimhigher" target="_blank" rel="nofollow"><AimhigherLogo /></a>
   </footer>
 )
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Media from 'react-media'
 
 import Logo from '../images/logo.svg';
 import './header.scss';
 
 const Header = () => (
-  <header>
-      <Link to="/"><Logo /></Link>
-      <Link to="/about">About</Link>
-      <Link to="/weddings">Weddings</Link>
-      <Link to="/funerals">Funerals</Link>
-      <Link to="/other-services">Other Services</Link>
-      <Link to="/faq">FAQ</Link>
-      <Link to="/contact">Contact</Link>
-  </header>
+	<header>
+		<Link to="/" className="logo"><Logo /></Link>
+		<nav id="menu">
+			<Link to="/about">About</Link>
+			<Link to="/weddings">Weddings</Link>
+			<Link to="/funerals">Funerals</Link>
+			<Link to="/other-services">Other Services</Link>
+			<Link to="/faq">FAQ</Link>
+			<Link to="/contact">Contact</Link>
+		</nav>
+	</header>
 )
 
 export default Header
