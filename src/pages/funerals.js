c4d6cd4f6a284fbad90a6e6906de7a00c7f0aa6a
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-09-12 14:13:44 +0800

    added content to other pages

--- src/pages/funerals.js
+++ src/pages/funerals.js
@@ -5,6 +5,12 @@ import Layout from '../components/layout'
 const Funerals = () => (
   <Layout>
     <h1>Funerals</h1>
+    <p>With compassion and attention to detail, I will ensure your loved ones are remembered as they deserve to be.</p>
+    <p>A Funeral Service is one that helps the departed’s loved ones celebrate their life. So many would say that a happy service is certainly memorable. I find it very gratifying to provide my families with a funeral service that helps them with the grieving process.</p>
+    <p>Once again I take pride in my work providing a loving, caring service that many tell me after it is over, that is was a ‘Beautiful Service’. One elderly gentleman once told me that he hated going to funerals and being the age that he was (about 80) he went to many, but he enjoyed the service I had just given. He said this with a lovely smile.</p>
+    <p>The idea behind a Civil Celebrant doing a Funeral Service is so that it won’t be a religious service (or sermon) but some families still like to have the Lord’s Prayer, the 23rd Psalm or perhaps a short prayer or reading from the Bible included in the service and I have no difficulty in saying either one. Just the same, if a family doesn’t want any religious aspect to the service, I don’t have any adversity to that either.</p>
+    <p>With each service, I will meet the family where possible, and take down notes and stories on the deceased’s life. I then return to my computer and put together their ‘Story’. I will email it to a member of the family so that they can edit it in any way they feel necessary. This is very important as I must get it right every time. There is no ‘second time around’ with a Funeral. I always like to think that I have given something back to the family to help them in their grief and move on with their lives.</p>
+    <blockquote>Give your loved one the dignity they deserve with a caring and loving Funeral Service.</blockquote>
   </Layout>
 )
 
--- src/pages/other-services.js
+++ src/pages/other-services.js
@@ -5,6 +5,20 @@ import Layout from '../components/layout'
 const OtherServices = () => (
   <Layout>
     <h1>Other Services</h1>
+    <h2>Naming Ceremony</h2>
+    <p>This magical ceremony will give you and your family memories for a lifetime.</p>
+    <p>In today’s busy world many families don’t have time to attend a church, therefore when it comes to giving their child a christening, many churches won’t carry out a service if the family doesn’t attend the church all year round. Therefore, a Naming Ceremony is about the only way they can formalize the naming of the newest member of their family. That beautiful little bundle of joy who has to have a name to get them through life.</p>
+    <p>As a mother and grandmother who adores her offspring, I absolutely love carrying out a Naming Ceremony! Whether it is a small baby or young children, to carry out the service is very special to me. All children no matter what their age, have wonderful personalities and these can often be seen and encouraged throughout a Naming Ceremony.</p>
+    <p>I like to meet the parents and children and go through the requirements about a week before the ceremony, fine tuning the wording so that it is a very happy occasion.</p>
+    <p>As with my other services, if you would like a prayer or reading from the Bible, I am only too happy to comply with your wishes. However you may want a very close relative, God Parent or Guardian to read it for you.</p>
+    <p>I provide a Naming Certificate to each child so that it can be kept in the family records for years to come. There is no doubt that you will remember your child’s Naming Ceremony for many years.</p>
+    <h2>Renewal of Vows</h2>
+    <p>A Renewal of Vows ceremony is something that can be extremely special whether you have been married 50 years, 25 years or perhaps even one year. Yes even after a year very romantic and loving couples renew or reaffirm their vows in a loving and beautiful environment.</p>
+    <p>Perhaps you eloped numerous years ago because you didn’t want the huge affair that your parents were organizing or you didn’t want your parents to know that you were getting married. You may have been married overseas without family and friends present and now want to have a ceremony with them present. Or you may have had the situation where parents of either the Bride or Groom couldn’t be present for the big occasion so you now want to have a ceremony that includes them. How lovely would that be?</p>
+    <p>I have some beautiful ceremonies that you could choose from and you can even modify those ceremonies to make them unique to your requirements. With beautiful verses signifying your continued love for one another.</p>
+    <p>As with my weddings, I will have my sound system set up so that you can have the music of your choice and if desired, I will use the microphone so that no one misses a single word of this most beautiful event.</p>
+    <p>I will also provide you with a ‘Renewal of Vows’ certificate that you will want to have as a keepsake. Many of my couples frame their certificate to remind them of their devoted love to one another.</p>
+    <blockquote>Make your ‘Renewal of Vows’ ceremony one to remember with love.</blockquote>
   </Layout>
 )
 
