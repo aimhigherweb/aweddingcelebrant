788255e763517fb6f5e42caadc16e838da4457a3
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-09-12 13:53:52 +0800

    added about content

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,8 +3,16 @@ import React from 'react'
 import Layout from '../components/layout'
 
 const About = () => (
-  <Layout>
-    <h1>About Janne</h1>
+	<Layout>
+		<h1>About Janne</h1>
+		<p>With years of experience helping to make each ceremony you have, a very special one to remember</p>
+		<p>I am a very energetic and vibrant Celebrant who absolutely loves being in this wonderful industry. I adore carrying out the ceremonies and put in 100% effort with every single one! Each ceremony I do is just as important as the very first one I did a few years ago.</p>
+		<p>I decided to become a Civil Celebrant because I was bored with my job at the time and I decided that I wanted to be my own boss and work in an industry that gave me a fabulous sense of achievement.</p>
+		<p>I have been married since 1973 to the same wonderful man and have raised two handsome sons who have blessed me with three of the most loveable and beautiful grandchildren in the world. (Well that’s my opinion!)</p>
+		<p>What more could I want but to be involved in joining other couples in marriage, wishing them a lifetime of amazing memories and happiness, naming beautiful babies and children, or to help grieving families come to terms with the loss of their loved ones.</p>
+		<p>To me, being a Celebrant, has been my best career choice and one that I look forward to every day! My friends tell me that they have never seen in me, the passion I have now, for my career.</p>
+		<p>I approach each day with gusto and can’t wait for the next ceremony!</p>
+		<blockquote>Your Ceremony Your Way!</blockquote>
   </Layout>
 )
 
