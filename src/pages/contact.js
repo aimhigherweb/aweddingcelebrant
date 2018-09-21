21aa1f829c5b23e846164261824bb3a864868c3d
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-09-21 10:54:44 +0800

    desktop contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,28 +11,28 @@ const Contact = () => (
       <input type="hidden" name="bot-field" />
       <input type="hidden" name="form-name" value="contact" />
       <label htmlFor="name">
-          Name
-          <input type="text" name="name"/>
+          Name *
+          <input type="text" name="name" required />
       </label>
       <label htmlFor="email">
-          Email Address
-          <input type="email" name="email"/>
+          Email Address *
+          <input type="email" name="email" required />
       </label>
       <label htmlFor="phone">
           Contact Number
           <input type="text" name="phone" />
       </label>
       <label htmlFor="date">
-          Service Date and Time
-          <input type="text" name="date"/>
+          Service Date & Time *
+          <input type="text" name="date" required />
       </label>
       <label htmlFor="location">
-          Service Location
-          <input type="text" name="location"/>
+          Service Location *
+          <input type="text" name="location" required />
       </label>
       <label htmlFor="message">
           Notes
-          <textarea name="message"></textarea>
+          <textarea name="message" rows="8"></textarea>
       </label>
       <button type="submit">Submit</button>
     </form>
