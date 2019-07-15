4f1adedc5432a34aad5bc45ed69dd2692cfa9297
Amy Kapernick <amy@aimhigherwebdesign.com>
2019-07-15 20:50:18 +0800

    added to check window

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,61 +14,69 @@ const meta = {
 }
 
 // eslint-disable-next-line one-var
-const Contact = () => (
-		<Layout meta={meta}>
-			<h1>Contact</h1>
-			<div className="split">
-				<form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-					<input type="hidden" name="bot-field" />
-					<input type="hidden" name="form-name" value="contact" />
-					<label htmlFor="name">
-						Name *
-						<input type="text" name="name" required />
-					</label>
-					<label htmlFor="email">
-						Email Address *
-						<input type="email" name="email" required />
-					</label>
-					<label htmlFor="phone">
-						Contact Number
-						<input type="text" name="phone" />
-					</label>
-					<label htmlFor="date">
-						Service Date & Time *
-						<input type="text" name="date" required />
-					</label>
-					<label htmlFor="location">
-						Service Location *
-						<input type="text" name="location" required />
-					</label>
-					<label htmlFor="message">
-						Notes
-						<textarea name="message" rows="8"></textarea>
-					</label>
-					<button type="submit">Submit</button>
-				</form>
-				<div>
-					<p>
-						<strong>Phone: </strong>{' '}
-						<a href="tel:+61894018427" target="_blank">
-							08 9401 8427
-						</a>
-					</p>
-					<p>
-						<strong>Mobile: </strong>{' '}
-						<a href="tel:+61418955198" target="_blank">
-							0418 955 198
-						</a>
-					</p>
-					<Map style="mapbox://styles/mapbox/streets-v11" center={[115.748482, -31.799778]} zoom={[10]}>
-						<Marker coordinates={[115.748482, -31.799778]} anchor="bottom">
-							<MapPin />
-						</Marker>
-					</Map>
+const Contact = () => {
+		let ProdMap
+		if (typeof window !== `undefined`) {
+			ProdMap = () => (
+				<Map style="mapbox://styles/mapbox/streets-v11" center={[115.748482, -31.799778]} zoom={[10]}>
+					<Marker coordinates={[115.748482, -31.799778]} anchor="bottom">
+						<MapPin />
+					</Marker>
+				</Map>
+			)
+		}
+		return (
+			<Layout meta={meta}>
+				<h1>Contact</h1>
+				<div className="split">
+					<form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+						<input type="hidden" name="bot-field" />
+						<input type="hidden" name="form-name" value="contact" />
+						<label htmlFor="name">
+							Name *
+							<input type="text" name="name" required />
+						</label>
+						<label htmlFor="email">
+							Email Address *
+							<input type="email" name="email" required />
+						</label>
+						<label htmlFor="phone">
+							Contact Number
+							<input type="text" name="phone" />
+						</label>
+						<label htmlFor="date">
+							Service Date & Time *
+							<input type="text" name="date" required />
+						</label>
+						<label htmlFor="location">
+							Service Location *
+							<input type="text" name="location" required />
+						</label>
+						<label htmlFor="message">
+							Notes
+							<textarea name="message" rows="8"></textarea>
+						</label>
+						<button type="submit">Submit</button>
+					</form>
+					<div>
+						<p>
+							<strong>Phone: </strong>{' '}
+							<a href="tel:+61894018427" target="_blank">
+								08 9401 8427
+							</a>
+						</p>
+						<p>
+							<strong>Mobile: </strong>{' '}
+							<a href="tel:+61418955198" target="_blank">
+								0418 955 198
+							</a>
+						</p>
+						<ProdMap />
+					</div>
 				</div>
-			</div>
-		</Layout>
-	),
+			</Layout>
+		)
+	},
 	Map = ReactMapboxGl({
 		accessToken: process.env.GATSBY_MAPBOX_API,
 	})
